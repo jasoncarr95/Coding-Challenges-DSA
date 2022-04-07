b23d99d372a877c2c372f67ede0e4c3edaf2414e
jasoncarr95 <jason@jasoncarr.dev>
2022-04-07 18:27:29 -0400

    thursday.. dumib af today :(

---
+++ js/thurs4-7.js
@@ -0,0 +1,27 @@
+// function pattern(n){
+//     ????????????????????
+        //     let output = 1;
+        //     if (n>1){for (let i = 2; i<=n; i++){
+        //         output+= `\n1\n${i}`
+        //     }}
+            
+        //     return output;
+        //    }
+
+        //    console.log(pattern(3))
+        //    console.log(pattern(10))
+  
+//  
+// function first (arr,n){
+//     let result = [];
+//     if (n === 0) {
+//         return result
+//     }else {
+//         for(let i=0; i<n; i++){
+//             result.push(arr[i])
+//         }
+//       return result
+//     }
+// }
+// console.log(first (['a','b','c','d','e'],2 ) );
+
