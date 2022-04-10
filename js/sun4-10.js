29096c5f6ec530b1098642b2edaa692a802dfa80
jasoncarr95 <jason@jasoncarr.dev>
2022-04-10 15:29:26 -0400

    .

--- js/sun4-10.js
+++ js/sun4-10.js
@@ -18,3 +18,24 @@
         }
         return total;
       }
+
+// SUM OF NUMEROURS ARGUMENTS
+      //MY SOLUTION
+      function findSum(){
+        let args = Array.from(arguments)
+        
+        if(args.length === 0){return 0}
+        else {
+            return args.every(num => num >= 0) ? args.reduce( (a,b) => a+b) : -1;
+            
+        }
+      }
+      //BETTER PRACTICE
+    function findSum(...nums) {
+        return nums.reduce((a, b) => a < 0 || b < 0 ? - 1 : a + b, 0)
+      }
+
+      console.log(findSum(1,3,5) ); //9
+      console.log(findSum(0,3,9,2) ); //14
+      console.log(findSum( 1,-2,4) ); //-1
+      console.log(findSum() );
