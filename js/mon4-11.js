c45b192a8fc58b701b7fec545e30ac5cfe7f1be7
jasoncarr95 <jason@jasoncarr.dev>
2022-04-11 15:17:41 -0400

    leveled up to kata 5 ayyee

---
+++ js/mon4-11.js
@@ -0,0 +1,21 @@
+//function take argument of list of words. return length of longest word
+    // function longest(words) {
+    //     words.sort( (a,b) => b.length - a.length)
+    //     return words[0].length
+    // }
+
+    // console.log( longest(['simple', 'is', 'better', 'than', 'complex'])); //7
+
+//  
+
+// function doubleChar(str) {
+//     let doubleStr=''
+//     for(let i=0; i<=str.length; i++){
+//         doubleStr+=str[i]+str[i]
+//     }
+//     return doubleStr
+//   }
+
+//   console.log( doubleChar('string')); //'ssttrriinngg'
+
+
