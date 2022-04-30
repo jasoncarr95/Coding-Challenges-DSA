dedb8f391e8d16d85d80d3978c177349c3c2e95d
jasoncarr95 <jason@jasoncarr.dev>
2022-04-29 22:32:46 -0400

    ya boyyy

---
+++ js/4-29Fri.js
@@ -0,0 +1,20 @@
+// function arrayDiff(a, b) {
+//     return a.filter((e) => !b.includes(e))
+// }
+
+// function spinWords(string){
+//     let words = Array.from(string)
+//     words.forEach(word=>{
+//        if (word.length>=5)word.split('').reverse().join('')
+//    })
+//    return words.join('')
+// }
+
+//   console.log(spinWords("Welcome"))
+//   console.log(spinWords("This is rehtona test"))
+
+
+var isSquare = function(n){
+    return Number.isInteger(Math.sqrt(n))
+}
+console.log(isSquare(3));
