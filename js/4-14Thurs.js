f30164fd1d9cf965a268073269e3aa58ecfe41f5
jasoncarr95 <jason@jasoncarr.dev>
2022-05-15 17:23:31 -0400

    date

--- js/4-14Thurs.js
+++ js/4-14Thurs.js
@@ -2,3 +2,4 @@ function reverseWords(str) {
   return str.split(' ').map((word)=> word.split('').reverse().join('')).join(' ')
 }
 console.log( reverseWords('The quick brown fox jumps over the lazy dog.'))
+
