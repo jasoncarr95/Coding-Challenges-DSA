2106bef6ecc65ab414cce881f1104f550c597e7c
jasoncarr95 <jason@jasoncarr.dev>
2022-05-26 21:21:09 -0400

    thurs :(

---
+++ 5-26Thurs.js/counting-duplicates.js
@@ -0,0 +1,14 @@
+//  COUNTING DUPLICATES   KYU 6
+//  Parmeters:  given a string that will contain either uppercase/lowercase letters or numeric digits
+//  Return: count of DISTINCT CASE-SENSITIVE characters that ooccur more than once
+//  Example:
+    console.log(duplicateCount("aabbcde")); //  2
+    console.log(duplicateCount("aabBcde")); //  1
+
+//  PseudoCode:
+    //  split string into an array
+    // check each element
+
+function duplicateCount(text){
+  //...
+}
---
+++ 5-26Thurs.js/index.js
@@ -0,0 +1,21 @@
+//  TITLE   KYU #
+//  Parmeters:
+//  Return:
+//  Example:
+//  PseudoCode:
+
+// function sumStrings(a,b) {
+//     a = a.split('').map((e) => Number(e));
+//     b = b.split('').map((e) => Number(e));
+
+//     if(a.length > b.length){
+
+//     }
+    
+
+//     result = a.map((e,i) => e+= b[i])
+//     return result.join('');
+    
+// }
+
+//  console.log(sumStrings('123','456'));
