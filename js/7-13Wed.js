16ab916e0b9dcf4c20f1f52887c38c4510a06f9d
jasoncarr95 <jason@jasoncarr.dev>
2022-07-13 20:49:47 -0400

    wed

---
+++ js/7-13Wed.js
@@ -0,0 +1,16 @@
+//  IS THERE A VOWEL IN THERE   KYU 8
+    //  given an array of nuymbers
+    //  check if any numbers are charcodes for lowercase vowels --not Y
+    // if yes, change the array value to a string of that value
+    //  return resulting array
+
+function isVow(a){
+    const vowCodes = "aeiou".split('').map(e => e.charCodeAt())
+    
+    return a.map(function(e){
+        return vowCodes.includes(e) ? String.fromCharCode(e) : e
+    })
+}
+
+
+
