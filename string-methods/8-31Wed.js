fadd6df6ee5a0b6f9263259b4e894c0f0cafd59a
jasoncarr95 <jason@jasoncarr.dev>
2022-08-31 17:55:12 -0400

    Anotha one

--- string-methods/8-31Wed.js
+++ string-methods/8-31Wed.js
@@ -7,4 +7,7 @@ function multiTable(number) {
     return result.slice(0,-1)
 }
 
-
+// Alan Partridge II - Apple Turnover   KYU 8
+function apple(x){
+    return (Number(x) **2) > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
