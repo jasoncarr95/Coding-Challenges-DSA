84cc02e8ed290f084769c7ea1352371d16e2fc15
jasoncarr95 <jason@jasoncarr.dev>
2022-08-31 17:51:08 -0400

    String challenge for wed 8/31

---
+++ string-methods/8-31Wed.js
@@ -0,0 +1,10 @@
+// Multiplication table for number  KYU 8
+function multiTable(number) {
+    let result = ""
+    for (i = 1; i <= 10; i++) {
+        result+= `${i} * ${number} = ${i * number}\n`;
+    }
+    return result.slice(0,-1)
+}
+
+
