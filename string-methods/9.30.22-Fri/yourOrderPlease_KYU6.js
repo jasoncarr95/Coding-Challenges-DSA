8a71cab2c2edce51f425fcc554e42fb24b63c16d
jasoncarr95 <jason@jasoncarr.dev>
2022-09-30 21:24:02 -0400

    Coding challenges Fri. 9.30.22 - String Methods

---
+++ string-methods/9.30.22-Fri/yourOrderPlease_KYU6.js
@@ -0,0 +1,15 @@
+function order(words) {
+    function getNumberFromString(s) {
+        return Number(s.replace(/\D/g, ""));
+    }
+
+    return words
+        .split(" ")
+        .sort((a, b) => {
+            let numA = getNumberFromString(a);
+            let numB = getNumberFromString(b);
+
+            return numA - numB;
+        })
+        .join(" ");
+}
