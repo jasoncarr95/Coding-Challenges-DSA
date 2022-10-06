4efa70777945f81e5c138e22771e30041489c7c6
jasoncarr95 <jason@jasoncarr.dev>
2022-10-06 14:02:56 -0400

    Add basic stack data-structure example

---
+++ data-structures/stack.js
@@ -0,0 +1,6 @@
+const stack = [];
+stack.push(2); // stack is now [2]
+stack.push(5); // stack is now [2,5]
+let i = stack.pop(); // stack is now [2]
+console.log(`i = ${i}`); // 5
+console.log(`stack = ${stack}`) // [2]
