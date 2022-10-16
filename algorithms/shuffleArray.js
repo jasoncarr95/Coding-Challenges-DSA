3ef08df271a93218f1b49bc0f573b4804764ee50
jasoncarr95 <jason@jasoncarr.dev>
2022-10-16 14:08:42 -0400

    Add shuffleArray algorithm

---
+++ algorithms/shuffleArray.js
@@ -0,0 +1,18 @@
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+};
+
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(shuffleArray(myArray)); // [[8,3,5],[2,4,9],[6,7,1]]
