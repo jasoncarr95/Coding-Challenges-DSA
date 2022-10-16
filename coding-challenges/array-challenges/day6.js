8d161f549058ba47e56decebb1df02bf294e5988
jasoncarr95 <jason@jasoncarr.dev>
2022-10-16 14:07:56 -0400

    Finish week 2 array coding challenges
    Add sliding puzzle

---
+++ coding-challenges/array-challenges/day6.js
@@ -0,0 +1,10 @@
+// Day 6 (Oct 14. fri) Completed: Sun. 10.16.22
+// last challenge of week
+// plug array functions into a skeleton react project to create a sliding puzzle game
+
+/* TASKS
+    1. fork codepen
+    2. add link to github/twitter
+    3. insert daily functions 
+    4. play!
+*/
---
+++ coding-challenges/array-challenges/sliding-puzzle-bob/README.md
@@ -0,0 +1,4 @@
+# Sliding Puzzle - To Fill In
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jsoncarr/pen/GRdLgNY](https://codepen.io/jsoncarr/pen/GRdLgNY).
+
