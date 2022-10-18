38fe11159807d0517c6382152a86da9107611f39
jasoncarr95 <jason@jasoncarr.dev>
2022-10-18 17:04:27 -0400

    Add validAnagram challenge

--- 242.validAnagram.js
+++ 242.validAnagram.js
@@ -5,10 +5,18 @@
  * @return {boolean}
  * if valid anagram return true
  */
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
     // check inputs first
-    if(s.length !== t.length) return false
+    if (s.length !== t.length) return false;
+
+    return (
+        s.toLowerCase().split("").sort().join("") ==
+        t.toLowerCase().split("").sort().join("")
+    );
 };
 
-console.log(isAnagram("anagram","nagaram")) //true
-console.log(isAnagram("rat", "car"))
+console.log(isAnagram("anagram", "nagaram")); //true
+console.log(isAnagram("rat", "car"));
+
+isAnagram("anagram", "nagaram");
+isAnagram("rat", "car");
