52f242484c8bf416ffc520e524a67aa03cc869f3
jasoncarr95 <jason@jasoncarr.dev>
2022-11-30 19:14:52 -0500

    Coding challenges  Wed. 11.30.22

---
+++ KYU_6/pokemonDamageCalculator.js
@@ -0,0 +1,11 @@
+// @ts-check
+/**
+ *
+ * @param {string} yourType
+ * @param {string} opponentType
+ * @param {number} attack
+ * @param {number} defense
+ */
+function calculateDamage(yourType, opponentType, attack, defense) {}
+
+module.exports = calculateDamage;
---
+++ KYU_7/absentVowel.js
@@ -0,0 +1,34 @@
+// PSEUDO
+// create variable to hold vowels [a,e,i,o,u]
+// iterate through string, if doenst include our vowel then return index of missing vowel
+// split&filter str into array of vowels
+// vowelArr => return index of missing vowel
+
+/**
+ *
+ * @param {string} str will contain all vowels except for 1
+ * @return {number} index of the missing vowel
+ */
+function absentVowel(str) {
+    const vowels = {
+        a: 0,
+        e: 1,
+        i: 2,
+        o: 3,
+        u: 4,
+    };
+    const vowelArray = str.split("").filter((char) => char in vowels);
+
+    for (const letter in vowels) {
+        if (!vowelArray.includes(letter)) return vowels[letter];
+    }
+}
+
+console.log(
+    absentVowel("John Doe hs seven red pples under his bsket"),
+    `=>  0  ; missing: "a"`
+);
+console.log(
+    absentVowel("Bb Smith sent us six neatly arranged range bicycles"),
+    `=>  3  ; missing: "o"`
+);
