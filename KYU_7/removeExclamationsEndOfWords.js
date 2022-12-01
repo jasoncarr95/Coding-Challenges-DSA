929d20cc986eb409b25ed51fc18eec7374796894
jasoncarr95 <jason@jasoncarr.dev>
2022-12-01 15:54:08 -0500

    Coding Challenges Thurs. 12.1.22

---
+++ KYU_7/removeExclamationsEndOfWords.js
@@ -0,0 +1,23 @@
+/**
+ * Remove all exclamation marks from the end of words.
+ * Words are separated by a single space.
+ * There are no exclamation marks within a word.
+ * @param {string} str sentence... words separated by spaces
+ * @returns {string} Removed all exclamation marks from the end of words.
+ */
+
+function remove(str) {
+    const words = str
+        .split(" ")
+        .map((word) =>
+            word.endsWith("!") ? word.slice(0, word.indexOf("!")) : word
+        );
+    return words;
+}
+
+console.log(remove("Hi!"), "=> Hi");
+console.log(remove("Hi!!!"), "=> Hi");
+console.log(remove("!Hi"), "=> !Hi");
+console.log(remove("!Hi!"), "=> !Hi");
+console.log(remove("Hi! Hi!"), "=> Hi Hi");
+console.log(remove("!!!Hi !!hi!!! !hi"), "=> !!!Hi !!hi !hi");
