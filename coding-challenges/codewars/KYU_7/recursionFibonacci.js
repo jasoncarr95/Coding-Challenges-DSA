70d27a6e8639d4ba3bbf7af9d46ffd3ce6300536
jasoncarr95 <jason@jasoncarr.dev>
2023-01-18 17:58:00 -0500

    Coding challenges  Wed. 1.18.23

---
+++ coding-challenges/codewars/KYU_7/recursionFibonacci.js
@@ -0,0 +1,21 @@
+/**
+ * Fibonacci: f(n) = f(n-1) + f(n-2)
+ * f(1) = 1
+ * f(2) = 1
+ */
+
+/**
+ * Returns the nth number in fibonacci sequence
+ * @param {number} n the nth number in fibonacci sequence
+ * @returns {number} returns f(n)
+ */
+const fibonacci = (n) => {
+    if (n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+console.log(fibonacci(4), 3);
+console.log(fibonacci(1), 1);
+console.log(fibonacci(5), 5);
+console.log(fibonacci(6), 8);
+console.log(fibonacci(10));
---
+++ coding-challenges/huntober/daily-challenges/recursionFactorial.js
@@ -0,0 +1,16 @@
+// Task: Create the function factorial that receives n and returns n!
+
+/**
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+function factorial(n) {
+    // use recursion, 0! => 1
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5), 120);
+console.log(factorial(1), 1);
+console.log(factorial(3), 6); // 3 * 2 * 1
