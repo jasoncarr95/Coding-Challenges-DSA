b68e160398e1d24589bc03945dd064b7d1bc0c3f
Jason Carr <jason@jasoncarr.dev>
2023-04-14 19:37:51 -0400

    Add SQL codewars dir & 3 challenges

---
+++ coding-challenges/codewars/sql/8-KYU/adultsOnly.sql
@@ -0,0 +1,10 @@
+-- In your application, there is a section for adults only. You need to get a list of names and ages of users from the users table, who are 18 years old or older.
+
+-- users table schema
+--     name
+--     age
+
+SELECT name,
+  age
+FROM users
+WHERE age >= 18
---
+++ coding-challenges/codewars/sql/8-KYU/onTheCanadianBorder.sql
@@ -0,0 +1,4 @@
+SELECT name,
+  country
+FROM travelers
+WHERE country NOT IN ('Canada', 'Mexico', 'USA')
